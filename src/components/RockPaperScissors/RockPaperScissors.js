7a7f08dad6364ef984ae9339bb9a54a983be5fb3
Clodus-NT <clodus.nt@gmail.com>
2022-10-12 13:01:19 -0400

    initial commit

---
+++ src/components/RockPaperScissors/RockPaperScissors.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function RockPaperScissors() {
+  return (
+    <div>
+      <h1>Rock-Paper-Scissors</h1>
+      <h2>Computer Choice:<span id='computer-choice'></span></h2>
+    </div>
+  )
+}
