d57ad2ccbcb320333ecbd29451c1ed75232d636c
Clodus-NT <clodus.nt@gmail.com>
2022-10-12 15:58:42 -0400

    added routing with a switch case statement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 import RockPaperScissors from './components/RockPaperScissors/RockPaperScissors';
+import MemoryMatch  from './components/MemoryMatch/MemoryMatch';
+import WhacAMole from './components/WhacAMole/WhacAMole';
+import Breakout from './components/Breakout/Breakout';
+import Frogger from './components/Frogger/Frogger';
+import ConnectFour from './components/ConnectFour/ConnectFour';
+import SpaceInvaders from './components/SpaceInvaders/SpaceInvaders';
 
 function App() {
+  let component;
+  switch (window.location.pathname) {
+    case '/':
+      component = <Home />
+      break
+    case '/rock-paper-scissors':
+      component = <RockPaperScissors />
+      break
+    case '/memory-match':
+      component = <MemoryMatch />
+      break
+    case '/whac-a-mole':
+      component = <WhacAMole />
+      break
+    case '/breakout':
+      component  = <Breakout />
+      break
+    case '/frogger':
+      component = <Frogger />
+      break
+    case '/connect-four':
+      component = <ConnectFour />
+      break
+    case '/space-invaders':
+      component = <SpaceInvaders />
+      break
+  }
+
   return (
     <div className="App">
       <Navbar />
-      <RockPaperScissors />
+      {component}
     </div>
   );
 }
